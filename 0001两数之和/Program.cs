474c9a2cd3776d20673a13649ca56c0d9ca7b8ec
wuhuajiecsharp <17681550310@163.com>
2019-02-26 18:44:11 +0800

    create project and push 0001 and 0002 project

---
+++ 0001两数之和/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0001两数之和
+{
+    class Program
+    {
+        /*
+         * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+
+            你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+
+            示例:
+
+                给定 nums = [2, 7, 11, 15], target = 9
+
+                因为 nums[0] + nums[1] = 2 + 7 = 9
+                所以返回 [0, 1]
+         */
+        static void Main(string[] args)
+        {
+            int[] nums = new[] { 3, 3 };
+            int target = 6;
+            int[] numIndex = TwoSum(nums, target);
+            for (int i = 0; i < numIndex.Length; i++)
+            {
+                Console.WriteLine(numIndex[i]);
+            }
+
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// 获取对应的数组下标
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        private static int[] GetIndex(int[] nums, int target)
+        {
+            int[] indexInts = new int[2];
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        indexInts[0] = i;
+                        indexInts[1] = j;
+                    }
+                }
+            }
+
+            return indexInts;
+        }
+
+        private static int[] TwoSum(int[] nums, int target)
+        {
+            Dictionary<int, int> dictionary = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                dictionary.Add(i, nums[i]);
+            }
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int temp = target - nums[i];
+                if (dictionary.ContainsValue(temp))
+                {
+                    var keys = dictionary.Where(d => d.Value == temp).Select(d => d.Key).Max();
+                    if (keys != i)
+                    {
+                        return new int[] {i, keys};
+                    }
+                }
+            }
+            throw new Exception("没有该值相加的元素");
+        }
+
+    }
+}
---
+++ 0002两数相加/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0002两数相加
+{
+    class Program
+    {
+        //给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
+
+        //如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
+
+        //您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+
+        //示例：
+
+        //输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+        //    输出：7 -> 0 -> 8
+        //原因：342 + 465 = 807
+        static void Main(string[] args)
+        {
+            ListNode l1 = new ListNode(9);
+            l1.next = new ListNode(9);
+
+            ListNode l2 = new ListNode(9);
+            ListNode l3 = AddTwoNumbers(l1, l2);
+        }
+
+
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int x) { val = x; }
+        }
+
+
+        public static ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            ListNode returnListNode = new ListNode(0);
+            ListNode p1 = l1, p2 = l2, curNode = returnListNode;
+            int carry = 0;
+            while (p1 != null || p2 != null)
+            {
+                int sum = (p1 == null ? 0 : p1.val) + (p2 == null ? 0 : p2.val) + carry;
+                carry = sum / 10;
+                curNode.next = new ListNode(sum % 10);
+                curNode = curNode.next;
+                if (p1 != null)
+                {
+                    p1 = p1.next;
+                }
+
+                if (p2 != null)
+                {
+                    p2 = p2.next;
+                }
+            }
+
+            if (carry == 1)
+            {
+                curNode.next = new ListNode(carry);
+            }
+
+            return returnListNode.next;
+        }
+
+
+    }
+}
